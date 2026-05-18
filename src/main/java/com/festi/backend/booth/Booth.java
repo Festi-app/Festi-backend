513e06de4204076cadf1f63b1ae644d14819fcf3
younha <cyh051205@gmail.com>
2026-05-18 19:44:43 +0900

    fix/domain entities 불필요한 ManyToOne -> OneToOne 수정

--- src/main/java/com/festi/backend/booth/Booth.java
+++ src/main/java/com/festi/backend/booth/Booth.java
@@ -12,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.AccessLevel;
@@ -30,7 +31,7 @@ public class Booth extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "manager_id")
     private User manager;
 
