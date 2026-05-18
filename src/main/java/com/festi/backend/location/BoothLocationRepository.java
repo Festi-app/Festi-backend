416e3d44d33a9b697ad0ac09dd1705f8f81cebe3
Lapis0875 <lapis0875@gmail.com>
2026-05-18 18:33:46 +0900

    feat: Booth Entity의 N+1 문제 해결

--- src/main/java/com/festi/backend/location/BoothLocationRepository.java
+++ src/main/java/com/festi/backend/location/BoothLocationRepository.java
@@ -3,11 +3,14 @@ package com.festi.backend.location;
 import com.festi.backend.booth.BoothType;
 import java.time.LocalDate;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoothLocationRepository extends JpaRepository<BoothLocation, Short> {
 
+    @EntityGraph(attributePaths = "booth")
     List<BoothLocation> findByDayAndTypeOrderByIndex(LocalDate day, BoothType type);
 
+    @EntityGraph(attributePaths = "booth")
     List<BoothLocation> findByDayOrderByIndex(LocalDate day);
 }
